598c9a2eaa6f211afc45c925046a0b58ecc9149a
KISLY74 <kisly.nikita@mail.ru>
2022-08-14 15:34:54 +0300

    feat: create queries of axios

---
+++ src/http/userApi.js
@@ -0,0 +1,24 @@
+import axios from "axios"
+
+const API_URL = "http://localhost:1710"
+const config = {
+  headers: {
+    'Authorization': `Bearer ${localStorage.getItem('token')}`
+  }
+}
+
+export const regin = async (email, password, username) => {
+  const data = await axios.post(`${API_URL}/api/user/regin`, { email, password, username })
+  localStorage.setItem('token', data.token)
+  return data
+}
+export const login = async (email, password) => {
+  const data = await axios.post(`${API_URL}/api/user/login`, config, { email, password })
+  localStorage.setItem('token', data.token)
+  return data
+}
+export const check = async () => {
+  const data = await axios.get(`${API_URL}/api/user/login`, config)
+  localStorage.setItem('token', data.token)
+  return data
+}
