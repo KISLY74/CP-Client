df2bd1cb1cdc0a76a46f13d68cbfc88a6afd6770
KISLY74 <kisly.nikita@mail.ru>
2022-08-15 15:09:52 +0300

    feat: add routes to admin and user personal pages

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -1,12 +1,16 @@
 import { Routes, Route, Navigate } from "react-router-dom"
 import Authorization from "../pages/Authorization"
-import { REGIN_ROUTE, LOGIN_ROUTE } from "../utils/routes"
+import { REGIN_ROUTE, LOGIN_ROUTE, ADMIN_ROUTE, USER_ROUTE } from "../utils/routes"
+import Admin from "../pages/Admin"
+import User from "../pages/User"
 
 const AppRouter = () => {
   return (
     <Routes>
       <Route path={REGIN_ROUTE} element={<Authorization />} />
       <Route path={LOGIN_ROUTE} element={<Authorization />} />
+      <Route path={ADMIN_ROUTE} element={<Admin />} />
+      <Route path={USER_ROUTE} element={<User />} />
       <Route path="*" element={<Navigate to="/login" />} />
     </Routes>
   )
