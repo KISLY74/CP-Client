34d3580930f14f9ae769824b01dec9b6305faa1a
KISLY74 <kisly.nikita@mail.ru>
2022-08-16 12:16:09 +0300

    fix: change way of get token, name variable and replace wrong route of check query

--- src/http/userApi.js
+++ src/http/userApi.js
@@ -8,19 +8,19 @@ const config = {
 }
 
 export const regin = async (email, password, username) => {
-  const data = await axios.post(`${API_URL}/api/user/regin`, { email, password, username })
-  localStorage.setItem('token', data.token)
-  return data
+  const res = await axios.post(`${API_URL}/api/user/regin`, { email, password, username })
+  localStorage.setItem('token', res.data.token)
+  return res
 }
 export const login = async (email, password) => {
-  const data = await axios.post(`${API_URL}/api/user/login`, { email, password })
-  localStorage.setItem('token', data.token)
-  return data
+  const res = await axios.post(`${API_URL}/api/user/login`, { email, password })
+  localStorage.setItem('token', res.data.token)
+  return res
 }
 export const check = async () => {
-  const data = await axios.get(`${API_URL}/api/user/login`, config)
-  localStorage.setItem('token', data.token)
-  return data
+  const res = await axios.get(`${API_URL}/api/user/auth`, config)
+  localStorage.setItem('token', res.data.token)
+  return res
 }
 export const getOneUser = async (email) => {
   const data = await axios.post(`${API_URL}/api/user/get`, { email })
