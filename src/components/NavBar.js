7027842e346ed2657400178dd19fe081b30fa864
KISLY74 <kisly.nikita@mail.ru>
2022-08-16 12:31:42 +0300

    feat: add handle click on btn logout

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,17 +1,27 @@
 import { Button, Navbar } from "react-bootstrap"
 import Nav from "react-bootstrap/Nav"
 import { useNavigate } from "react-router-dom"
+import { LOGIN_ROUTE } from "../utils/routes"
+import { useContext } from "react"
+import { Context } from "../index"
+import { observer } from "mobx-react-lite"
 
-const NavBar = () => {
+const NavBar = observer(() => {
+  const { user } = useContext(Context)
   const history = useNavigate()
+  const logOut = () => {
+    user.setIsAuth(false)
+    localStorage.clear()
+    history(LOGIN_ROUTE)
+  }
   return (
     <Navbar className="d-flex justify-content-between" style={{ paddingLeft: 15, paddingRight: 15 }
     } bg="dark" variant="dark" >
       <Nav style={{ color: 'white' }}>
-        <Button variant={"outline-light"} onClick={() => history("/login")} >Авторизация</Button>
+        <Button variant={"outline-light"} onClick={() => logOut()}>{user.isAuth ? "Выйти" : "Авторизация"}</Button>
       </Nav>
     </Navbar >
   )
-}
+})
 
 export default NavBar
