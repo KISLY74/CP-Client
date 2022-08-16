218d1e703232292fb16e81e16029d64bbaaecaef
KISLY74 <kisly.nikita@mail.ru>
2022-08-17 00:27:13 +0300

    feat: add routes depend on public or authorizated users

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -3,17 +3,40 @@ import Authorization from "../pages/Authorization"
 import { REGIN_ROUTE, LOGIN_ROUTE, ADMIN_ROUTE, USER_ROUTE } from "../utils/routes"
 import Admin from "../pages/Admin"
 import User from "../pages/User"
+import { observer } from "mobx-react-lite"
+import { useContext } from "react"
+import { Context } from ".."
 
-const AppRouter = () => {
+const authRoutes = [
+  {
+    path: ADMIN_ROUTE,
+    component: <Admin />
+  },
+  {
+    path: USER_ROUTE,
+    component: <User />
+  }
+]
+
+const publicRoutes = [
+  {
+    path: REGIN_ROUTE,
+    component: <Authorization />
+  },
+  {
+    path: LOGIN_ROUTE,
+    component: <Authorization />
+  }
+]
+
+const AppRouter = observer(() => {
+  const { user } = useContext(Context)
   return (
     <Routes>
-      <Route path={REGIN_ROUTE} element={<Authorization />} />
-      <Route path={LOGIN_ROUTE} element={<Authorization />} />
-      <Route path={ADMIN_ROUTE} element={<Admin />} />
-      <Route path={USER_ROUTE} element={<User />} />
-      <Route path="*" element={<Navigate to="/login" />} />
+      {user.isAuth ? authRoutes.map(e => <Route path={e.path} element={e.component} />) : publicRoutes.map(e => <Route path={e.path} element={e.component} />)}
+      <Route path="*" element={<Navigate to={user.isAuth ? ADMIN_ROUTE : LOGIN_ROUTE} />} />
     </Routes>
   )
-}
+})
 
 export default AppRouter
