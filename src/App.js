62f5a26678bba5d2548c61e946424f62c67ad4f2
KISLY74 <kisly.nikita@mail.ru>
2022-08-16 12:31:15 +0300

    feat: add check user is authorizated only one time when update component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import AppRouter from "./components/AppRouter"
 import { BrowserRouter } from "react-router-dom"
 import NavBar from "./components/NavBar";
+import { observer } from "mobx-react-lite";
+import { useEffect, useContext } from "react"
+import { Context } from "./index"
+import { check } from "./http/userApi"
 
-function App() {
+const App = observer(() => {
+  const { user } = useContext(Context)
+  useEffect(() => {
+    check().then(() => {
+      user.setIsAuth(true)
+      user.setUser(true)
+    }).catch(() => user.setIsAuth(false))
+  }, [])
   return (
     <BrowserRouter>
       <NavBar />
       <AppRouter />
     </BrowserRouter>
   );
-}
+})
 
 export default App;
