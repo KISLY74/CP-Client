7332b09e85350fcc4aa2e0fd21675955649fddf0
KISLY74 <kisly.nikita@mail.ru>
2022-08-17 18:31:56 +0300

    feat: add spinner to display loading registration and authorization, check user is authorizate and set false that logout when he push back on the routes

--- src/pages/Authorization.js
+++ src/pages/Authorization.js
@@ -1,8 +1,8 @@
 import { NavLink, useLocation, useNavigate } from "react-router-dom";
-import { useContext, useState } from "react";
-import { Container, Card, Form, Button, Row } from "react-bootstrap"
+import { useContext, useEffect, useState } from "react";
+import { Container, Card, Form, Button, Row, Spinner } from "react-bootstrap"
 import { login, regin, getOneUser } from "../http/userApi"
-import { ADMIN_ROUTE, USER_ROUTE } from "../utils/routes";
+import { ADMIN_ROUTE, LOGIN_ROUTE, USER_ROUTE } from "../utils/routes";
 import { observer } from "mobx-react-lite";
 import { Context } from "../index";
 
@@ -13,16 +13,15 @@ const Authorization = observer(() => {
   const [username, setUserName] = useState('')
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const [loading, setLoading] = useState(true)
   const history = useNavigate()
   const checkStatusAuth = async () => {
     await getOneUser(email).then((res) => {
       if (res.data.status === "Block") {
         user.setIsAuth(false)
         user.setIsBlock(true)
-        alert("Вы заблокированы")
       } else {
         user.setIsAuth(true)
-        alert("Успешно авторизован")
       }
       user.setUser(res.data)
       localStorage.setItem("username", res.data.username)
@@ -30,54 +29,70 @@ const Authorization = observer(() => {
     })
   }
   const handleClickReginLogin = async () => {
+    setLoading(false)
     try {
       if (isLogin) {
-        await login(email, password).then(() => checkStatusAuth()).catch((e) => alert(e.response.data.message))
+        user.setIsAuth(false)
+        await login(email, password).then(() => checkStatusAuth()).catch((e) => alert(e.response.data.message)).finally(() => setLoading(true))
       } else {
-        await regin(email, password, username).then(() => alert("Успешно зарегистрирован")).catch((e) => alert(e.response.data.message))
+        await regin(email, password, username).then(() => history(LOGIN_ROUTE)).catch((e) => alert(e.response.data.message)).finally(() => setLoading(true))
       }
     } catch (e) {
       console.log(e)
     }
   }
   return (
-    <Container
-      className="d-flex justify-content-center align-items-center"
-      style={{ height: window.innerHeight - 54 }}
-    >
-      <Card style={{ width: 600 }} className="p-5">
-        <h2 className="m-auto">{isLogin ? "Авторизация" : "Регистрация"}</h2>
-        <Form className="d-flex flex-column">
-          {!isLogin ? <Form.Control
-            className="mt-3"
-            placeholder="Имя пользователя"
-            value={username}
-            onChange={(e) => setUserName(e.target.value)}
-          /> : ""}
-          <Form.Control
-            className="mt-3"
-            placeholder="Введите email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <Form.Control
-            className="mt-3"
-            placeholder="Введите пароль"
-            value={password}
-            type="password"
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <Row className="d-flex justify-content-between mt-3">
-            {isLogin ? <div>Нет аккаунта?
-              <NavLink style={{ textDecoration: "none" }} to="/regin"> Зарегистрируйтесь!</NavLink>
-            </div> : <div>Есть аккаунт?
-              <NavLink style={{ textDecoration: "none" }} to="/login"> Войдите!</NavLink>
-            </div>}
-            <Button className="mt-3" variant={"outline-success"} onClick={() => handleClickReginLogin()}>{isLogin ? "Войти" : "Зарегистрироваться"}</Button>
-          </Row>
-        </Form>
-      </Card>
-    </Container >
+    <div>
+      {localStorage.clear()}
+      {user.isAuth ? user.setIsAuth(false) :
+        <Container
+          className="d-flex justify-content-center align-items-center"
+          style={{ height: window.innerHeight - 54 }}
+        >
+          <Card style={{ width: 600 }} className="p-5">
+            <h2 className="m-auto">{isLogin ? "Авторизация" : "Регистрация"}</h2>
+            <Form className="d-flex flex-column">
+              {!isLogin ? <Form.Control
+                className="mt-3"
+                placeholder="Имя пользователя"
+                value={username}
+                onChange={(e) => setUserName(e.target.value)}
+              /> : ""}
+              <Form.Control
+                className="mt-3"
+                placeholder="Введите email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <Form.Control
+                className="mt-3"
+                placeholder="Введите пароль"
+                value={password}
+                type="password"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <Row className="d-flex justify-content-between mt-3">
+                {isLogin ? <div>Нет аккаунта?
+                  <NavLink style={{ textDecoration: "none" }} to="/regin"> Зарегистрируйтесь!</NavLink>
+                </div> : <div>Есть аккаунт?
+                  <NavLink style={{ textDecoration: "none" }} to="/login"> Войдите!</NavLink>
+                </div>}
+                <Button className="mt-3" variant={"outline-success"} onClick={() => handleClickReginLogin()}>{isLogin ? "Войти" : "Зарегистрироваться"}</Button>
+              </Row>
+            </Form>
+            {loading ? "" : <Button className="mt-3 d-flex justify-content-center" variant="dark">
+              <Spinner
+                as="span"
+                animation="grow"
+                size="sm"
+                role="status"
+                aria-hidden="true"
+              />
+              Loading...
+            </Button>}
+          </Card>
+        </Container >}
+    </div>
   );
 })
 
