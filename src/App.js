c4f4390d368c15861bfc9e3906d4f40ba76cb239
KISLY74 <kisly.nikita@mail.ru>
2022-08-17 18:23:51 +0300

    feat: add Spinner when check user is authorizate

--- src/App.js
+++ src/App.js
@@ -2,17 +2,22 @@ import AppRouter from "./components/AppRouter"
 import { BrowserRouter } from "react-router-dom"
 import NavBar from "./components/NavBar";
 import { observer } from "mobx-react-lite";
-import { useEffect, useContext } from "react"
+import { useEffect, useContext, useState } from "react"
 import { Context } from "./index"
 import { check } from "./http/userApi"
+import { Spinner } from "react-bootstrap"
 
 const App = observer(() => {
   const { user } = useContext(Context)
+  const [loading, setLoading] = useState(true)
   useEffect(() => {
     check().then(() => {
       user.setIsAuth(true)
-    }).catch(() => user.setIsAuth(false))
+    }).catch(() => user.setIsAuth(false)).finally(() => setLoading(false))
   }, [])
+  if (loading) {
+    return <Spinner className="position-absolute top-50 start-50" animation="border" />
+  }
   return (
     <BrowserRouter>
       <NavBar />
