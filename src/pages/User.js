b29df65eecc4d8b80b952c05ee66ab5b6b410544
KISLY74 <kisly.nikita@mail.ru>
2022-08-22 20:53:09 +0300

    feat: create form to display creating collection

--- src/pages/User.js
+++ src/pages/User.js
@@ -1,6 +1,32 @@
+import { Form, Button } from "react-bootstrap"
+
 const User = () => {
   return (
-    <h4>Личная страница пользователя</h4>
+    <div className="d-flex">
+      <Form className="p-3 d-flex" style={{ minWidth: 450, rowGap: 14, flexDirection: "column" }}>
+        <h4>Создание коллекции</h4>
+        <Form.Group>
+          <Form.Label>Название коллекции</Form.Label>
+          <Form.Control type="text" placeholder="Введите название коллекции" />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Описание коллекции</Form.Label>
+          <Form.Control as="textarea" rows={3} />
+        </Form.Group>
+        <Form.Group >
+          <Form.Label>Тема коллекции</Form.Label>
+          <Form.Select>
+            <option>Книги</option>
+            <option>Фильмы</option>
+            <option>Автомобили</option>
+          </Form.Select>
+        </Form.Group>
+        <Button>Создать коллекцию</Button>
+      </Form>
+      <div className="p-3">
+        <h4>Список коллекций</h4>
+      </div>
+    </div >
   )
 }
 
