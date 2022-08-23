148ba44c1eae02882fabe136fd3452cc84438ef2
KISLY74 <kisly.nikita@mail.ru>
2022-08-23 23:02:45 +0300

    feat: add states to get value of form controls for create collection, create handleClick fot createCollection, create update method which update list of collections, add spinner to loading list collections, set user in store from localStorage

--- src/pages/User.js
+++ src/pages/User.js
@@ -1,33 +1,69 @@
-import { Form, Button } from "react-bootstrap"
+import { useContext, useEffect, useState } from "react"
+import { Form, Button, Card, ListGroup, Spinner } from "react-bootstrap"
+import { createCollection, getCollectionById, getCollectionsByUser } from "../http/collectionApi"
+import { Context } from "../index"
+import { observer } from "mobx-react-lite"
 
-const User = () => {
+const User = observer(() => {
+  const { user } = useContext(Context)
+  const [collectionName, setCollectionName] = useState('')
+  const [description, setDescription] = useState('')
+  const [theme, setTheme] = useState('Книги')
+  const [collections, setCollections] = useState([])
+  const [loading, setLoading] = useState(true)
+  const handleClickCreateCollection = async () => {
+    await createCollection(collectionName, description, theme, user.email)
+    updateCollectionsUser()
+  }
+  const updateCollectionsUser = async () => {
+    const data = await getCollectionsByUser(user.email)
+    setCollections(data)
+  }
+  useEffect(() => {
+    setLoading(false)
+    user.setUser(JSON.parse(localStorage.getItem('userStore')))
+    updateCollectionsUser().finally(() => setLoading(true))
+  }, [])
   return (
     <div className="d-flex">
       <Form className="p-3 d-flex" style={{ minWidth: 450, rowGap: 14, flexDirection: "column" }}>
         <h4>Создание коллекции</h4>
         <Form.Group>
           <Form.Label>Название коллекции</Form.Label>
-          <Form.Control type="text" placeholder="Введите название коллекции" />
+          <Form.Control type="text" value={collectionName} onChange={(e) => setCollectionName(e.target.value)} placeholder="Введите название коллекции" />
         </Form.Group>
         <Form.Group>
           <Form.Label>Описание коллекции</Form.Label>
-          <Form.Control as="textarea" rows={3} />
+          <Form.Control as="textarea" rows={3} value={description} onChange={(e) => setDescription(e.target.value)} />
         </Form.Group>
         <Form.Group >
           <Form.Label>Тема коллекции</Form.Label>
-          <Form.Select>
+          <Form.Select value={theme} onChange={(e) => setTheme(e.target.value)}>
             <option>Книги</option>
             <option>Фильмы</option>
             <option>Автомобили</option>
           </Form.Select>
         </Form.Group>
-        <Button>Создать коллекцию</Button>
+        <Button onClick={() => handleClickCreateCollection()}>Создать коллекцию</Button>
       </Form>
       <div className="p-3">
         <h4>Список коллекций</h4>
-      </div>
+        <div className="d-flex mt-4" style={{ columnGap: 20, flexWrap: "wrap" }}>
+          {loading ?
+            collections.map((e, i) =>
+              <Card className="mb-4" style={{ minWidth: 300, maxWidth: 300 }} key={i}>
+                <Card.Body>
+                  <Card.Title>{e.name}</Card.Title>
+                </Card.Body>
+                <ListGroup>
+                  <ListGroup.Item>Описание: {e.description}</ListGroup.Item>
+                  <ListGroup.Item>Тема: {e.theme}</ListGroup.Item>
+                </ListGroup>
+              </Card>) : <Spinner className="position-absolute top-50 start-50" animation="border" />}
+        </div>
+      </div >
     </div >
   )
-}
+})
 
 export default User
