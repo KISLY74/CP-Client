c982f1130b44505790eba8770f0475a14cd38a33
KISLY74 <kisly.nikita@mail.ru>
2022-08-24 17:56:20 +0300

    feat: create methods to api for create, delete, get all items and get items by collection

---
+++ src/http/itemApi.js
@@ -0,0 +1,17 @@
+import axios from "axios"
+import { API_URL } from "./userApi"
+
+export const createCollection = async (name, tags, id) => {
+  await axios.post(`${API_URL}/api/item/create`, { name, tags, id })
+}
+export const getItems = async () => {
+  const data = await axios.get(`${API_URL}/api/item/get/all`)
+  return data.data
+}
+export const getItemsByCollection = async (id) => {
+  const data = await axios.post(`${API_URL}/api/item/getAllByCollection`, { id })
+  return data.data
+}
+export const deleteItem = async (id) => {
+  await axios.put(`${API_URL}/api/item/delete`, { id })
+}
