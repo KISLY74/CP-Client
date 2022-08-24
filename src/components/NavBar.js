b3959e1b071ec0bdfaac9446e276fe66f9ddaa77
KISLY74 <kisly.nikita@mail.ru>
2022-08-24 10:10:11 +0300

    feat: add check to has admin role to display on navbar admin page

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,7 @@ import { Button, Navbar } from "react-bootstrap"
 import Nav from "react-bootstrap/Nav"
 import { NavLink, useNavigate } from "react-router-dom"
 import { USER_ROUTE, LOGIN_ROUTE, ADMIN_ROUTE } from "../utils/routes"
-import { useContext } from "react"
+import { useContext, useEffect } from "react"
 import { Context } from "../index"
 import { observer } from "mobx-react-lite"
 
@@ -14,13 +14,16 @@ const NavBar = observer(() => {
     localStorage.clear()
     history(LOGIN_ROUTE)
   }
+  useEffect(() => {
+    user.setUser(JSON.parse(localStorage.getItem('userStore')))
+  }, [])
   return (
     <Navbar className="d-flex justify-content-between" style={{ paddingLeft: 15, paddingRight: 15 }
     } bg="dark" variant="dark" >
       <h4 className="text-white">{user.isAuth ? localStorage.getItem('username') : ""}</h4>
       {user.isAuth ?
         <Nav style={{ color: 'white' }}>
-          <NavLink className="nav-link" to={ADMIN_ROUTE}>Администратор</NavLink>
+          {user.roles ? user.roles.includes("ADMIN") ? <NavLink className="nav-link" to={ADMIN_ROUTE}>Администратор</NavLink> : false : user.setUser(JSON.parse(localStorage.getItem('userStore')))}
           <NavLink className="nav-link" to={USER_ROUTE}>Пользователь</NavLink>
           <Button variant={"outline-light"} onClick={() => logOut()}>{user.isAuth ? "Выйти" : "Авторизация"}</Button>
         </Nav> :
