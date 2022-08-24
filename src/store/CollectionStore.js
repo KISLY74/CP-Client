06e58e3bfb6efa2cc987f80db1fcae2c853a8c43
KISLY74 <kisly.nikita@mail.ru>
2022-08-24 16:36:22 +0300

    feat: create store for collection

---
+++ src/store/CollectionStore.js
@@ -0,0 +1,17 @@
+import { makeAutoObservable } from "mobx"
+
+export default class CollectionStore {
+  constructor() {
+    this._collection = {}
+    makeAutoObservable(this)
+  }
+  setCollection(collection) {
+    this._collection = collection
+  }
+  get collection() {
+    return this._collection
+  }
+  get collectionName() {
+    return this._collection.name
+  }
+}
