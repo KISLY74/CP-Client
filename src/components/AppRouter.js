c8e96fbe549c2cd26aa765ae9c148f6f144ec1f3
KISLY74 <kisly.nikita@mail.ru>
2022-08-24 20:35:23 +0300

    feat: add route to app of item page

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -1,9 +1,10 @@
 import { Routes, Route, Navigate } from "react-router-dom"
 import Authorization from "../pages/Authorization"
-import { REGIN_ROUTE, LOGIN_ROUTE, ADMIN_ROUTE, USER_ROUTE, COLLECTION_ROUTE } from "../utils/routes"
+import { REGIN_ROUTE, LOGIN_ROUTE, ADMIN_ROUTE, USER_ROUTE, COLLECTION_ROUTE, ITEM_ROUTE } from "../utils/routes"
 import Admin from "../pages/Admin"
 import User from "../pages/User"
 import Collection from "../pages/Collection"
+import Item from "../pages/Item"
 
 const AppRouter = () => {
   return (
@@ -13,6 +14,7 @@ const AppRouter = () => {
       <Route path={ADMIN_ROUTE} element={<Admin />} />
       <Route path={USER_ROUTE} element={<User />} />
       <Route path={COLLECTION_ROUTE} element={<Collection />} />
+      <Route path={ITEM_ROUTE} element={<Item />} />
       <Route path="*" element={<Navigate to={LOGIN_ROUTE} />} />
     </Routes>
   )
