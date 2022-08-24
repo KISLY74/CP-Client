248be6155509a7bf5d21da5ab2a03bb60caf8d4e
KISLY74 <kisly.nikita@mail.ru>
2022-08-24 16:38:29 +0300

    feat: create route to collection page

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -1,8 +1,9 @@
 import { Routes, Route, Navigate } from "react-router-dom"
 import Authorization from "../pages/Authorization"
-import { REGIN_ROUTE, LOGIN_ROUTE, ADMIN_ROUTE, USER_ROUTE } from "../utils/routes"
+import { REGIN_ROUTE, LOGIN_ROUTE, ADMIN_ROUTE, USER_ROUTE, COLLECTION_ROUTE } from "../utils/routes"
 import Admin from "../pages/Admin"
 import User from "../pages/User"
+import Collection from "../pages/Collection"
 
 const AppRouter = () => {
   return (
@@ -11,6 +12,7 @@ const AppRouter = () => {
       <Route path={LOGIN_ROUTE} element={<Authorization />} />
       <Route path={ADMIN_ROUTE} element={<Admin />} />
       <Route path={USER_ROUTE} element={<User />} />
+      <Route path={COLLECTION_ROUTE} element={<Collection />} />
       <Route path="*" element={<Navigate to={LOGIN_ROUTE} />} />
     </Routes>
   )
