607a81ee1277d97e78e9ca263be1bd3c8bac37a0
KISLY74 <kisly.nikita@mail.ru>
2022-08-25 13:18:55 +0300

    feat: set biggest collections to collection store

--- src/App.js
+++ src/App.js
@@ -3,14 +3,21 @@ import { BrowserRouter } from "react-router-dom"
 import NavBar from "./components/NavBar";
 import { observer } from "mobx-react-lite";
 import { useEffect, useContext, useState } from "react"
+import { getCollections } from "./http/collectionApi";
 import { Context } from "./index"
 import { check } from "./http/userApi"
 import { Spinner } from "react-bootstrap"
 
 const App = observer(() => {
-  const { user } = useContext(Context)
+  const { user, collection } = useContext(Context)
   const [loading, setLoading] = useState(true)
+  const getCollectionsAndSetToStore = async () => {
+    await getCollections().then((data) => {
+      collection.setBiggest(JSON.stringify(data.sort((a, b) => b.items.length - a.items.length).slice(0, 5)))
+    })
+  }
   useEffect(() => {
+    getCollectionsAndSetToStore()
     check().then(() => {
       user.setIsAuth(true)
     }).catch(() => user.setIsAuth(false)).finally(() => setLoading(false))
