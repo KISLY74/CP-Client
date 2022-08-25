71af2c1105ede2b5c8ce906c4906426c7f90e8f1
KISLY74 <kisly.nikita@mail.ru>
2022-08-25 19:57:45 +0300

    feat: get and set to item store last addtion items

--- src/App.js
+++ src/App.js
@@ -6,18 +6,23 @@ import { useEffect, useContext, useState } from "react"
 import { getCollections } from "./http/collectionApi";
 import { Context } from "./index"
 import { check } from "./http/userApi"
+import { getItems } from "./http/itemApi";
 import { Spinner } from "react-bootstrap"
 
 const App = observer(() => {
-  const { user, collection } = useContext(Context)
+  const { user, collection, item } = useContext(Context)
   const [loading, setLoading] = useState(true)
   const getCollectionsAndSetToStore = async () => {
     await getCollections().then((data) => {
       collection.setBiggest(JSON.stringify(data.sort((a, b) => b.items.length - a.items.length).slice(0, 5)))
     })
   }
+  const getItemsAndSetToStore = async () => {
+    await getItems().then((data) => item.setLastAdditionItems(JSON.stringify(data.sort((a, b) => new Date(b.dateAddition) - new Date(a.dateAddition)).slice(0, 5))))
+  }
   useEffect(() => {
     getCollectionsAndSetToStore()
+    getItemsAndSetToStore()
     check().then(() => {
       user.setIsAuth(true)
     }).catch(() => user.setIsAuth(false)).finally(() => setLoading(false))
