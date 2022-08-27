647e16adc73d1e8e471c12f1a344de9eafe42929
KISLY74 <kisly.nikita@mail.ru>
2022-08-27 11:17:07 +0300

    feat: add route search result page to app

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -1,11 +1,12 @@
 import { Routes, Route, Navigate } from "react-router-dom"
 import Authorization from "../pages/Authorization"
-import { REGIN_ROUTE, LOGIN_ROUTE, ADMIN_ROUTE, USER_ROUTE, COLLECTION_ROUTE, ITEM_ROUTE, MAIN_ROUTE } from "../utils/routes"
+import { REGIN_ROUTE, LOGIN_ROUTE, ADMIN_ROUTE, USER_ROUTE, COLLECTION_ROUTE, ITEM_ROUTE, MAIN_ROUTE, SEARCH_RESULTS_ROUTE } from "../utils/routes"
 import Admin from "../pages/Admin"
 import User from "../pages/User"
 import Collection from "../pages/Collection"
 import Item from "../pages/Item"
 import Main from "../pages/Main"
+import SearchResults from "../pages/SearchResults"
 
 const AppRouter = () => {
   return (
@@ -17,6 +18,7 @@ const AppRouter = () => {
       <Route path={COLLECTION_ROUTE} element={<Collection />} />
       <Route path={ITEM_ROUTE} element={<Item />} />
       <Route path={MAIN_ROUTE} element={<Main />} />
+      <Route path={SEARCH_RESULTS_ROUTE} element={<SearchResults />} />
       <Route path="*" element={<Navigate to={LOGIN_ROUTE} />} />
     </Routes>
   )
