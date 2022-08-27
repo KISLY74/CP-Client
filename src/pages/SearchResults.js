bba4a21529e5789e7fd37cb3222e6e083231ceb3
KISLY74 <kisly.nikita@mail.ru>
2022-08-27 11:18:16 +0300

    feat: create search results component to display page of results search elements by tag

---
+++ src/pages/SearchResults.js
@@ -0,0 +1,49 @@
+import { useState } from "react"
+import { Form, FormGroup, Button, Spinner, Card, ListGroup } from "react-bootstrap"
+import { getItems } from "../http/itemApi"
+
+const SearchResults = () => {
+  const [tag, setTag] = useState("")
+  const [itemsByTag, setItemsByTag] = useState([])
+  const [loading, setLoading] = useState(true)
+  let items = []
+  const searchItemsByTag = async (tagInput) => {
+    setLoading(false)
+    await getItems().then((data) => data.map(e => {
+      e.tags = e.tags.map(el => el.trim(""))  
+      e.tags.map((str => str.includes(tagInput || tag) ? items.push(e) : false))
+    })).finally(() => setLoading(true))
+    setItemsByTag(items)
+  }
+  return (
+    <div className="p-3">
+      <h2>Страница результатов поиска</h2>
+      <Form>
+        <FormGroup>
+          <Form.Control value={tag} onChange={(e) => {
+            setTag(e.target.value)
+            searchItemsByTag(e.target.value)
+          }} className="fs-3" placeholder="Введите тег" type="text" />
+          <Button className="mt-2" variant="secondary" onClick={() => searchItemsByTag(tag)}>Поиск</Button>
+        </FormGroup>
+      </Form>
+      <div className="mt-2">
+        <h3>Результаты поиска</h3>
+        <div className="d-flex" style={{ flexWrap: "wrap", columnGap: 20 }}>
+          {loading ? itemsByTag.map((e, i) => <Card className="mb-4" style={{ minWidth: 300, maxWidth: 300 }} key={i}>
+            <Card.Body>
+              <Card.Title>
+                {e.name}
+              </Card.Title>
+            </Card.Body>
+            <ListGroup>
+              <ListGroup.Item>Теги: {e.tags.toString()}</ListGroup.Item>
+            </ListGroup>
+          </Card>) : <Spinner className="position-absolute top-50 start-50" animation="border" />}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default SearchResults
