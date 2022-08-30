b9ebca825357781acb1b680a9adf2753e3de27ff
KISLY74 <kisly.nikita@mail.ru>
2022-08-30 16:10:24 +0300

    feat: add data of edit mode, fields of replace collection, load collections to store

--- src/store/CollectionStore.js
+++ src/store/CollectionStore.js
@@ -5,16 +5,37 @@ export default class CollectionStore {
     this._collection = {}
     this._biggest = []
     this._additionalFields = []
+    this._isLoad = true
+    this._editMode = false
+    this._fields = {}
     makeAutoObservable(this)
   }
   setCollection(collection) {
     this._collection = collection
+  }
+  setFields(obj) {
+    this._fields = obj
+  }
+  setIsLoad(bool) {
+    this._isLoad = bool
+  }
+  setEditMode(bool) {
+    this._editMode = bool
   }
   setBiggest(collections) {
     this._biggest = collections
   }
   setAdditionalFields(fields) {
     this._additionalFields = fields
+  }
+  get isLoad() {
+    return this._isLoad
+  }
+  get editMode() {
+    return this._editMode
+  }
+  get fields() {
+    return this._fields
   }
   get collection() {
     return this._collection
