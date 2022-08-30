46841acc181704eb3ed8713d6635c30b1eb05094
KISLY74 <kisly.nikita@mail.ru>
2022-08-30 16:19:58 +0300

    feat: add condition of user is view to other users and add name of view user and none user

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,18 @@
 import { Button, Navbar } from "react-bootstrap"
 import Nav from "react-bootstrap/Nav"
-import { NavLink, useNavigate } from "react-router-dom"
-import { USER_ROUTE, LOGIN_ROUTE, ADMIN_ROUTE, MAIN_ROUTE, SEARCH_RESULTS_ROUTE } from "../utils/routes"
-import { useContext, useEffect } from "react"
+import { NavLink, useLocation, useNavigate } from "react-router-dom"
+import { LOGIN_ROUTE, ADMIN_ROUTE, OWN_PAGE_ROUTE, MAIN_ROUTE, SEARCH_RESULTS_ROUTE } from "../utils/routes"
+import { useContext, useEffect, useState } from "react"
 import { Context } from "../index"
 import { observer } from "mobx-react-lite"
 
 const NavBar = observer(() => {
   const { user } = useContext(Context)
+  const location = useLocation()
   const history = useNavigate()
   const logOut = () => {
     user.setIsAuth(false)
+    user.setIsView(false)
     localStorage.clear()
     history(LOGIN_ROUTE)
   }
@@ -20,18 +22,20 @@ const NavBar = observer(() => {
   return (
     <Navbar className="d-flex justify-content-between" style={{ paddingLeft: 15, paddingRight: 15 }
     } bg="dark" variant="dark" >
-      <h4 className="text-white">{user.isAuth ? localStorage.getItem('username') : ""}</h4>
-      {user.isAuth ?
-        <Nav style={{ color: 'white' }}>
-          {user.roles ? user.roles.includes("ADMIN") ? <NavLink className="nav-link" to={ADMIN_ROUTE}>Администратор</NavLink> : false : user.setUser(JSON.parse(localStorage.getItem('userStore')))}
-          <NavLink className="nav-link" to={MAIN_ROUTE}>Главная</NavLink>
-          <NavLink className="nav-link" to={USER_ROUTE}>Пользователь</NavLink>
-          <NavLink className="nav-link" to={SEARCH_RESULTS_ROUTE}>Результаты поиска</NavLink>
-          <Button variant={"outline-light"} onClick={() => logOut()}>{user.isAuth ? "Выйти" : "Авторизация"}</Button>
-        </Nav> :
-        <Nav style={{ color: 'white' }}>
-          <Button variant={"outline-light"} onClick={() => logOut()}>{user.isAuth ? "Выйти" : "Авторизация"}</Button>
-        </Nav>}
+      <h4 className="text-white">{user.isAuth ? localStorage.getItem('username') : "Гость"} {user.isView && location.pathname !== OWN_PAGE_ROUTE ? `(${JSON.parse(localStorage.getItem('viewUser')).username})` : ""}</h4>
+      <Nav>
+        <NavLink className="nav-link" to={MAIN_ROUTE}>Главная</NavLink>
+        <NavLink className="nav-link" to={SEARCH_RESULTS_ROUTE}>Результаты поиска</NavLink>
+        {user.isAuth ?
+          <Nav style={{ color: 'white' }}>
+            {user.roles ? user.roles.includes("ADMIN") ? <NavLink className="nav-link" to={ADMIN_ROUTE}>Администратор</NavLink> : false : user.setUser(JSON.parse(localStorage.getItem('userStore')))}
+            <NavLink className="nav-link" to={OWN_PAGE_ROUTE}>Пользователь</NavLink>
+            <Button variant={"outline-light"} onClick={() => logOut()}>{user.isAuth ? "Выйти" : "Авторизация"}</Button>
+          </Nav> :
+          <Nav style={{ color: 'white' }}>
+            <Button variant={"outline-light"} onClick={() => logOut()}>{user.isAuth ? "Выйти" : "Авторизация"}</Button>
+          </Nav>}
+      </Nav>
     </Navbar >
   )
 })
