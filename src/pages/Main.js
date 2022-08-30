0f2356884a230a186cce5c5ebeac6060af41dfba
KISLY74 <kisly.nikita@mail.ru>
2022-08-30 16:23:35 +0300

    feat: add handle click  of set user is view, push to route of user

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,27 +1,40 @@
 import { useContext, useEffect, useState } from "react"
-import { getCollections } from "../http/collectionApi"
-import { ListGroup, Card, Spinner, Container } from "react-bootstrap"
+import { ListGroup, Card, Button, Badge } from "react-bootstrap"
+import { USER_ROUTE } from "../utils/routes"
 import { observer } from "mobx-react-lite"
 import { Context } from "../index"
-import { getItems } from "../http/itemApi"
+import { useNavigate } from "react-router-dom"
+import { getUserByCollection } from "../http/userApi"
 
 const Main = observer(() => {
-  const { collection, item } = useContext(Context)
-  const [loading, setLoading] = useState(true)
+  const { collection, item, user } = useContext(Context)
+  const history = useNavigate()
+  const handleClickNormalUser = async (id) => {
+    await getUserByCollection(id).then((data) => {
+      user.setIsView(true)
+      history(USER_ROUTE)
+    })
+  }
+  useEffect(() => {
+    user.setIsView(false)
+  }, [])
   return (
     <div className="p-3">
       <h2>Главная страница</h2>
       <h4>Топ 5 самых больших коллекций</h4>
       <div className="d-flex" style={{ columnGap: 25, flexWrap: "wrap" }}>
         {collection.biggest.map((e) => <Card key={e._id} className="mb-4" style={{ minWidth: 300, maxWidth: 300 }} >
-          <Card.Body>
-            <Card.Title>
-              {e.name}
+          <Card.Body style={{ backgroundColor: '#222' }}>
+            <Card.Title >
+              <h4>
+                <Badge bg="success">{e.name}</Badge>
+              </h4>
             </Card.Title>
           </Card.Body>
           <ListGroup>
             <ListGroup.Item>Описание: {e.description}</ListGroup.Item>
             <ListGroup.Item>Тема: {e.theme}</ListGroup.Item>
+            <Button variant="secondary" onClick={() => handleClickNormalUser(e)}>Перейти к пользователю</Button>
           </ListGroup>
         </Card>)}
       </div>
