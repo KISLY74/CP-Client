5b6a9fe851b38fed4ac05ce5ea81505740ce3896
KISLY74 <kisly.nikita@mail.ru>
2022-08-30 16:20:57 +0300

    feat: add route to own page of user and props to user component

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -1,6 +1,6 @@
 import { Routes, Route, Navigate } from "react-router-dom"
 import Authorization from "../pages/Authorization"
-import { REGIN_ROUTE, LOGIN_ROUTE, ADMIN_ROUTE, USER_ROUTE, COLLECTION_ROUTE, ITEM_ROUTE, MAIN_ROUTE, SEARCH_RESULTS_ROUTE } from "../utils/routes"
+import { REGIN_ROUTE, LOGIN_ROUTE, ADMIN_ROUTE, USER_ROUTE, COLLECTION_ROUTE, ITEM_ROUTE, MAIN_ROUTE, SEARCH_RESULTS_ROUTE, OWN_PAGE_ROUTE } from "../utils/routes"
 import Admin from "../pages/Admin"
 import User from "../pages/User"
 import Collection from "../pages/Collection"
@@ -14,10 +14,11 @@ const AppRouter = () => {
       <Route path={REGIN_ROUTE} element={<Authorization />} />
       <Route path={LOGIN_ROUTE} element={<Authorization />} />
       <Route path={ADMIN_ROUTE} element={<Admin />} />
-      <Route path={USER_ROUTE} element={<User />} />
+      <Route path={USER_ROUTE} element={<User isOwn={false} />} />
       <Route path={COLLECTION_ROUTE} element={<Collection />} />
       <Route path={ITEM_ROUTE} element={<Item />} />
       <Route path={MAIN_ROUTE} element={<Main />} />
+      <Route path={OWN_PAGE_ROUTE} element={<User isOwn={true} />} />
       <Route path={SEARCH_RESULTS_ROUTE} element={<SearchResults />} />
       <Route path="*" element={<Navigate to={LOGIN_ROUTE} />} />
     </Routes>
