5b52c4499fc83297ada665abf3c795f8b0858aa1
KISLY74 <kisly.nikita@mail.ru>
2022-09-01 16:12:59 +0300

    feat: add isLoad for load list items after create and edit component click, editMode for on/off mode of editing for item, fields of item for get in ItemControlPanel component from ListItems component

--- src/store/ItemStore.js
+++ src/store/ItemStore.js
@@ -3,17 +3,41 @@ import { makeAutoObservable } from "mobx"
 export default class ItemStore {
   constructor() {
     this._item = {}
+    this._isLoad = true
+    this._editMode = false
+    this._fields = {}
     this._lastAdditionItems = []
     makeAutoObservable(this)
   }
   setItem(item) {
     this._item = item
   }
+  setIsLoad(bool) {
+    this._isLoad = bool
+  }
+  setEditMode(bool) {
+    this._editMode = bool
+  }
+  setFields(obj) {
+    this._fields = obj
+  }
   setLastAdditionItems(items) {
     this._lastAdditionItems = items
   }
   get item() {
     return this._item
+  }
+  get isLoad() {
+    return this._isLoad
+  }
+  get editMode() {
+    return this._editMode
+  }
+  get fields() {
+    return this._fields
+  }
+  get fieldsName() {
+    return this._fields.name
   }
   get lastAdditionItems() {
     return this._lastAdditionItems
