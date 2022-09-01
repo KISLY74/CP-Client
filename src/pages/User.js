984b4cf8693a60487e79d7a04fe0a8467dcb3e16
KISLY74 <kisly.nikita@mail.ru>
2022-09-01 22:28:54 +0300

    fix: remove div and not used import, replace field of store on localStorage

--- src/pages/User.js
+++ src/pages/User.js
@@ -1,7 +1,6 @@
 import { useContext, useEffect } from "react"
 import { Context } from "../index"
 import { observer } from "mobx-react-lite"
-import { LOGIN_ROUTE } from "../utils/routes"
 import { useNavigate } from "react-router-dom"
 import CollectionControlPanel from "../components/CollectionControlPanel"
 import ListCollection from "../components/ListCollections"
@@ -10,23 +9,21 @@ const User = observer((props) => {
   const { user, collection } = useContext(Context)
   const history = useNavigate()
   useEffect(() => {
-    props.isOwn ? user.setIsView(false) : user.setIsView(true)
+    props.isOwn ? localStorage.setItem('isView', false) : localStorage.setItem('isView', true)
   }, [])
   return (
     <div>
-      <div>
-        {props.isOwn ?
-          <div>
-            {collection.isLoad ?
-              <div>
-                <CollectionControlPanel />
-                <ListCollection isOwn={true} />
-              </div> : false}
-          </div> : <div>
-            {!user.isAuth ? user.setUser({ username: "Гость", roles: ["USER"] }) : user.roles.includes("ADMIN") ? <CollectionControlPanel /> : false}
-            {collection.isLoad ? <ListCollection isOwn={false} /> : false}
-          </div>}
-      </div>
+      {props.isOwn ?
+        <div>
+          {collection.isLoad ?
+            <div>
+              <CollectionControlPanel />
+              <ListCollection isOwn={true} />
+            </div> : false}
+        </div> : <div>
+          {!user.isAuth ? user.setUser({ username: "Гость", roles: ["USER"] }) : user.roles.includes("ADMIN") ? <CollectionControlPanel /> : false}
+          {collection.isLoad ? <ListCollection isOwn={false} /> : false}
+        </div>}
     </div >
   )
 })
