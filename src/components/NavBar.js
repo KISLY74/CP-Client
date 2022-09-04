8b5e6ac8ed5425d746bdec7fb7f9394e8b0ede26
KISLY74 <kisly.nikita@mail.ru>
2022-09-04 13:54:18 +0300

    feat: add adaptive menu of navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,6 @@
 import { Button, Navbar } from "react-bootstrap"
 import Nav from "react-bootstrap/Nav"
-import { NavLink, useLocation, useNavigate } from "react-router-dom"
+import { NavLink, useLocation, useNavigate, Link } from "react-router-dom"
 import { LOGIN_ROUTE, ADMIN_ROUTE, OWN_PAGE_ROUTE, MAIN_ROUTE, SEARCH_RESULTS_ROUTE } from "../utils/routes"
 import { useContext, useEffect, useState } from "react"
 import { Context } from "../index"
@@ -21,25 +21,30 @@ const NavBar = observer(() => {
     user.setUser(JSON.parse(localStorage.getItem('userStore')))
   }, [])
   return (
-    <Navbar className="d-flex justify-content-between" style={{ paddingLeft: 15, paddingRight: 15 }
+    <Navbar collapseOnSelect expand="sm" bg="dark" variant="dark" className="d-flex justify-content-between fixed-top" style={{ paddingLeft: 15, paddingRight: 15 }
     } bg="dark" variant="dark">
       <h4 className="text-white" id="username">
         {user.isAuth ? localStorage.getItem('username') : "Гость"}
         {localStorage.getItem('isView') === "true" && route !== MAIN_ROUTE && route !== SEARCH_RESULTS_ROUTE && route !== OWN_PAGE_ROUTE && route !== ADMIN_ROUTE ? `(${JSON.parse(localStorage.getItem('viewUser')).username})` : ""}
       </h4>
-      <Nav>
-        <NavLink className="nav-link" to={MAIN_ROUTE}>Главная</NavLink>
-        <NavLink className="nav-link" to={SEARCH_RESULTS_ROUTE}>Результаты поиска</NavLink>
-        {user.isAuth ?
-          <Nav style={{ color: 'white' }}>
-            {user.roles ? user.roles.includes("ADMIN") ? <NavLink className="nav-link" to={ADMIN_ROUTE}>Администратор</NavLink> : false : user.setUser(JSON.parse(localStorage.getItem('userStore')))}
-            <NavLink className="nav-link" to={OWN_PAGE_ROUTE}>Пользователь</NavLink>
-            <Button variant={"outline-light"} onClick={() => logOut()}>{user.isAuth ? "Выйти" : "Авторизация"}</Button>
-          </Nav> :
-          <Nav style={{ color: 'white' }}>
-            <Button variant={"outline-light"} onClick={() => logOut()}>{user.isAuth ? "Выйти" : "Авторизация"}</Button>
-          </Nav>}
-      </Nav>
+      <div>
+        <Navbar.Toggle aria-controls="navbarScroll" data-bs-toggle="collapse" data-bs-target="#navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav>
+            <NavLink className="nav-link" to={MAIN_ROUTE}>Главная</NavLink>
+            <NavLink className="nav-link" to={SEARCH_RESULTS_ROUTE}>Результаты поиска</NavLink>
+            {user.isAuth ?
+              <Nav style={{ color: 'white' }}>
+                {user.roles ? user.roles.includes("ADMIN") ? <NavLink className="nav-link" to={ADMIN_ROUTE}>Администратор</NavLink> : false : user.setUser(JSON.parse(localStorage.getItem('userStore')))}
+                <NavLink className="nav-link" to={OWN_PAGE_ROUTE}>Пользователь</NavLink>
+                <Button variant={"outline-light"} onClick={() => logOut()}>{user.isAuth ? "Выйти" : "Авторизация"}</Button>
+              </Nav> :
+              <Nav style={{ color: 'white' }}>
+                <Button variant={"outline-light"} onClick={() => logOut()}>{user.isAuth ? "Выйти" : "Авторизация"}</Button>
+              </Nav>}
+          </Nav>
+        </Navbar.Collapse>
+      </div>
     </Navbar >
   )
 })
